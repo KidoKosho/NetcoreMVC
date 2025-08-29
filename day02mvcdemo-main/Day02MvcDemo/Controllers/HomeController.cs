2c5471d48ef625c23dbb7d53149641b499bf3580
Quang <quangquan23451822@gmail.com>
2025-08-29 08:24:27 +0700

    :3

--- day02mvcdemo-main/Day02MvcDemo/Controllers/HomeController.cs
+++ day02mvcdemo-main/Day02MvcDemo/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace Day02MvcDemo.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
